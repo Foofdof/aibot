ef06f5c7391820d4d6a1fa1ca1a13e846db76803
Georgy Borohovski <foofdof@gmail.com>
2024-06-25 16:22:55 +0300

    new kb added

--- keyboards.py
+++ keyboards.py
@@ -8,3 +8,19 @@ def get_yes_no_kb() -> ReplyKeyboardMarkup:
     kb.button(text="Нет")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
+
+
+def menu_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Операции")
+    kb.button(text="Статистика")
+    kb.adjust(2)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def operations_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Добавить")
+    kb.button(text="Удалить")
+    kb.adjust(2)
+    return kb.as_markup(resize_keyboard=True)
