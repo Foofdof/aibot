7d30633f86d6f07c05ff58132650cf4f89308aba
Georgy Borohovski <foofdof@gmail.com>
2024-06-26 18:12:36 +0300

    operations handle updated

--- handlers.py
+++ handlers.py
@@ -1,9 +1,10 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
-from keyboards import get_yes_no_kb, menu_kb, operations_kb
+from aiogram.fsm.context import FSMContext
+from keyboards import menu_kb, operations_kb
 from dbmanager import *
-import datetime
+from states import OperationSq, Statistics
 
 router = Router()
 
@@ -17,33 +18,54 @@ async def cmd_start(message: Message):
 
 
 @router.message(F.text.lower() == "операции")
-async def operation(message: Message):
+async def operation(message: Message, state: FSMContext):
+    await state.set_state(OperationSq.operation_chosen)
     await message.answer(
         "Добавить или удалить?",
         reply_markup=operations_kb()
     )
 
 
-@router.message(F.text.lower() == "добавить")
-async def add_op(message: Message):
+@router.message(F.text.lower() == "добавить", OperationSq.operation_chosen)
+async def handle_add_operation(message: Message, state: FSMContext):
+    await state.set_state(OperationSq.amount)
+    await message.answer(
+        "Введите сумму",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@router.message(OperationSq.amount, F.text)
+async def add_operation(message: Message, state: FSMContext):
+    amount = message.text
+    await state.update_data(amount=amount)
+    await state.set_state(OperationSq.description)
+    await message.answer(
+        "Отлично, введите описание"
+    )
+
+
+@router.message(OperationSq.description, F.text)
+async def add_description(message: Message, state: FSMContext):
+    description = message.text
+    data = await state.update_data(description=description)
+    await state.clear()
+
     user_id = message.from_user.id
     db = DBManager(connection=sqlite3.connect('finance.db'))
 
-    q = """
-            CREATE TABLE IF NOT EXISTS operations (
-            id INTEGER,
-            date TIMESTAMP,
-            operation REAL,
-            description TEXT
-            )
-        """
-
-    db.create_table(q)
-    db.insert("operations", user_id, message.date, 300, "st")
-
+    table = {
+        "name": "operations",
+        "id": "INTEGER",
+        "date": "TIMESTAMP",
+        "operation": "REAL",
+        "description": "TEXT"
+    }
+    db.create_table(table)
+    db.insert("operations", user_id, message.date, data["amount"], data["description"])
     db.close()
 
     await message.answer(
-        "Жаль...",
-        reply_markup=ReplyKeyboardRemove()
+        "Отлично, операция добавлена",
+        reply_markup=menu_kb()
     )
