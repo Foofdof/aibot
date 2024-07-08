505c9d97a1125d5a38d039465d3ab2858b809b75
Georgy Borohovski <foofdof@gmail.com>
2024-07-08 22:51:20 +0300

    refactoring

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 from .commands import router as commands_router
 from .operations import router as operation_router
+from .statistics import router as stat_router
 
 router = Router(name=__name__)
 
 router.include_router(commands_router)
 router.include_router(operation_router)
+router.include_router(stat_router)
--- routers/operations/operations.py
+++ routers/operations/operations.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 from aiogram import Router, F
-from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
 
