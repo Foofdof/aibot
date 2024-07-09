b3c83709235de4c883cc7c7ec9a278b6bb942c06
Georgy Borohovski <foofdof@gmail.com>
2024-07-09 16:36:51 +0300

    menu command updated

--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -1,6 +1,8 @@
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+from magic_filter import F
 
 from keyboards import menu_kb
 
@@ -8,6 +10,10 @@ router = Router(name=__name__)
 
 
 @router.message(Command("start"))
+@router.message(Command("menu"))
+@router.message(CommandStart(
+    deep_link=True, magic=F.args == "start"
+))
 async def cmd_start(message: Message):
     await message.answer(
         "Что теперь?",
